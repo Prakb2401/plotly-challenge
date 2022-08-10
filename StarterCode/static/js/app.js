13367b2e831639a7198e188c7e1662df3e0fb4e5
Prakb2401 <prakb2401@gmail.com>
2022-08-10 19:00:53 -0400

    app.py work

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,8 @@
+// Plotly-Challenge
+
+// Url JSON link
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+function init(){
+    
+}
