b5888189df9962dc8585b7c720e6a8600eca1a2f
Prakb2401 <prakb2401@gmail.com>
2022-08-11 16:21:31 -0400

    ready for github pages

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,31 +1,85 @@
+// Linking JSON and URL
 const sample = `https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json`;
-
+// Console log data
 d3.json(sample).then(function(data1) {
 console.log(data1)
-
+// setting up variables and dropdown with names
 var Names1;
 Names1 = data1.names;
 let dropdownMenu = d3.select("#selDataset");
 Names1.forEach(name => dropdownMenu.append("option").text(name).property("value", name));
 });
 
-
+// If option in dropdown changes the rest of the website will change
 function optionChanged(id){
-    d3.json(sample).then(function(data2){
-    console.log(data2);
+    d3.json(sample).then(function(data1){
+    console.log(data1);
     var data_sample
-    data_sample = data2.samples
+    data_sample = data1.samples
     console.log(data_sample);
     var otu_ids = [];
     var otu_labels = []
     sample_values =[]
-    for(let i = 0; x < data_sample.length; i++){
+    // for loop to set up variables for plots
+    for(let i = 0; i < data_sample.length; i++){
         if(data_sample[i].id == id){
-            let otu_labels = data_sample[i].otulabels.slice(0,10).reverse();
+            let otu_labels = data_sample[i].otu_labels.slice(0,10).reverse();
             let sample_values = data_sample[i].sample_values.slice(0,10).reverse();
-            let oru_ids = data_sample[i].otu_ids;
-            let otu_labels2 = otu_labels;
-            
+            let otu_ids = data_sample[i].otu_ids;
+        //putting metadata into demographic info panel
+        let demo = d3.select("#sample-metadata").text(``);
+        data1.metadata.forEach(i => {
+            if(i.id == id) {
+                console.log(Object.keys(i));
+                Object.keys(i).forEach(x =>{
+                    key = x;
+                    info=(i[x]);
+                    let demo2 =d3.select("#sample-metadata").append("ul").text(`${key} : ${info}`)
+                })
+            }
+           
+        })
+        // bubble plot setup
+        let Bubble_plot= {
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids
+            },
+            text: otu_labels
+        }
+        let Bubble_plot2 = [Bubble_plot];
+
+        let bubbleAttributes = {
+            title: "OTUs by each sample",
+            xaxis: {title: 'OTU ID'},
+
+        };
+        // bubble plot creation
+        Plotly.newPlot("bubble", Bubble_plot2, bubbleAttributes);
+
+
+
+        // bar chart setup
+        let Barchart = {
+            x: sample_values,
+            y: otu_ids,
+            text: otu_labels,
+            type: "bar",
+            marker: {
+                color: "black",
+            },
+            orientation: "h"
+        };
+        let BarAttributes = {
+            title: "Top 10 OTU per sample",
+            yaxis:{title: "OTU ID", type: "category"}
+        };
+        let Barchart2 = [Barchart];
+        // Bar chart creation
+        Plotly.newPlot("bar", Barchart2, BarAttributes)
         };
     };
     });
